99d4cd210a17fd043ba1831e8ef48c1ec9191e66
ColemanDuPlessie <coleyduplessie@gmail.com>
2024-05-27 11:48:34 -0400

    (Theoretically) added equivariance

--- train_dqn.py
+++ train_dqn.py
@@ -19,11 +19,80 @@ BOARD_SIZE = 5
 ACTION_SPACE_SIZE = 8+(BOARD_SIZE-1)*4
 
 #Hyperparameters
+is_equivariant = False
 num_episodes  = 200000
 learning_rate = 0.0005
 gamma         = 0.98
 buffer_limit  = 50000
 batch_size    = 32
+     
+def make_equivariant(sample, board_size):
+    best_sample = {"wRook" : (999, 999), "wKing" : (999, 999), "bKing" : (999, 999)}
+    eq_code = None
+    max_coord = board_size-1
+    for vert_flip in (False, True):
+        for horiz_flip in (False, True):
+            for diag_flip in (False, True):
+                curr_sample = sample.copy()
+                for key in curr_sample.keys():
+                    if vert_flip:
+                        curr_sample[key] = (curr_sample[key][0], max_coord-curr_sample[key][1])
+                    if horiz_flip:
+                        curr_sample[key] = (max_coord-curr_sample[key][0], curr_sample[key][1])
+                    if diag_flip:
+                        curr_sample[key] = (curr_sample[key][1], curr_sample[key][0])
+                for key in curr_sample.keys():
+                    if curr_sample[key][0] < best_sample[key][0]:
+                        best_sample = curr_sample
+                        eq_code = (vert_flip, horiz_flip, diag_flip)
+                        break
+                    elif curr_sample[key][0] > best_sample[key][0]:
+                        break
+                    else:
+                        if curr_sample[key][1] < best_sample[key][1]:
+                            best_sample = curr_sample
+                            eq_code = (vert_flip, horiz_flip, diag_flip)
+                            break
+                        elif curr_sample[key][1] > best_sample[key][1]:
+                            break
+    return best_sample, eq_code
+
+def undo_equivariance(sample, eq_code, board_size):
+    max_coord = board_size-1
+    out = sample.copy()
+    for key in out.keys():
+        if eq_code[0]:
+            out[key] = (out[key][0], max_coord-out[key][1])
+        if eq_code[1]:
+            out[key] = (max_coord-out[key][0], out[key][1])
+        if eq_code[2]:
+            out[key] = (out[key][1], out[key][2])
+    return out
+
+def undo_equivariance_action(action_code, eq_code):
+    out = action_code
+    if action_code < 8: # King move
+        if eq_code[0]: # Vertical flip
+            out = (8-out)%8
+        if eq_code[1]: # Horizontal flip
+            out = ((out//4)*4+(4-out%4))%8
+        if eq_code[2]: # Diagonal flip
+            out = ((9-out)%8+1)%8
+    else: # Rook move
+        rook_move_size = board_size-1
+        dist = (out-8)%rook_move_size
+        direction = (out-8)//rook_move_size
+        if eq_code[0]: # Vertical flip
+            if direction == 1: direction = 3
+            elif direction == 3: direction = 1
+        if eq_code[1]: # Horizontal flip
+            if direction == 2: direction = 0
+            elif direction == 0: direction = 2
+        if eq_code[2]: # Diagonal flip
+            if direction % 2 == 0: direction += 1
+            else: direction -= 1
+        out = 8+direction*rook_move_size+dist
+    return out
 
 class ReplayBuffer():
     def __init__(self):
@@ -111,12 +180,18 @@ def main():
     for n_epi in range(num_episodes):
         epsilon = max(0.01, 0.08 - 0.01*(n_epi/200)) #Linear annealing from 8% to 1%
         obs, _ = env.reset()
+        if is_equivariant:
+            obs, eq_code = make_equivariant(obs, BOARD_SIZE)[0]
         s = np.concatenate(tuple(obs.values())) # This looks sketchy, but .values() is ordered based on order in which keys were created (which is always the same in my implementation of the environment) in python versions >= 3.6, so it's okay.
         done = False
 
         while not done:
-            a = q.sample_action(torch.from_numpy(s).float().to(device), epsilon)     
+            a = q.sample_action(torch.from_numpy(s).float().to(device), epsilon)
+            if is_equivariant:
+                a = undo_equivariance_action(a, eq_code)
             obs, r, done, truncated, info = env.step(a)
+            if is_equivariant:
+                obs, eq_code = make_equivariant(obs, BOARD_SIZE)[0]
             s_prime = np.concatenate(tuple(obs.values()))
             done_mask = 0.0 if done else 1.0
             memory.put((s,a,r/100.0,s_prime, done_mask))
