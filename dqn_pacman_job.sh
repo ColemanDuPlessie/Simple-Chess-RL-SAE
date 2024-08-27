458b69f715a2d7408d90b3c47ae02f74d5997168
Coleman DuPlessie <coleyduplessie@gmail.com>
2024-08-27 14:00:48 -0500

    Tried to run the right program on DELTA. Ran into a bug

--- dqn_pacman_job.sh
+++ dqn_pacman_job.sh
@@ -5,7 +5,7 @@
 #SBATCH --cpus-per-task=64           # number of cores per tasks
 #SBATCH --gres=gpu:4                 # number of gpus
 #SBATCH --time 4:00:00              # maximum execution time (HH:MM:SS)
-#SBATCH --output=delta_logs/train_rook_checkmate_DQN.out           # output file name
+#SBATCH --output=delta_logs/train_pacman_DQN.out           # output file name
 #SBATCH --partition=gpuA100x4
 #SBATCH --mem=208G
 #SBATCH --constraint="scratch"
