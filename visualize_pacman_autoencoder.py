8dcdbd1759f24b9953737f017e837b55862efa5b
ColemanDuPlessie <coleyduplessie@gmail.com>
2024-09-09 17:12:41 -0400

    Added visualization of autoencoder

--- visualize_pacman_autoencoder.py
+++ visualize_pacman_autoencoder.py
@@ -59,14 +59,24 @@ class ControlPanel:
         self.backstep_button = tk.Button(self.frame, text="Step reverse (SLOW!)", width=20, command=self.backstep)
         self.move_input = tk.Text(self.frame, width=2, height=1)
         self.step_manual = tk.Button(self.frame, text="Step in chosen direction", width=20, command=self.step_input)
+        self.canv_label = tk.Label(self.frame, text="DQN next move weights")
         self.canvas = tk.Canvas(self.frame, width=200, height=200, bg="#aabbcc")
+        self.autoencoder_canv_label = tk.Label(self.frame, text="Autoencoder next move weights")
+        self.autoencoder_canvas = tk.Canvas(self.frame, width=200, height=200, bg="#ffeedd")
+        self.diff_canv_label = tk.Label(self.frame, text="Autoencoder move weights minus DQN move weights")
+        self.diff_canvas = tk.Canvas(self.frame, width=200, height=200, bg="#ffffff")
         
         self.step_button.pack()
         self.manual_step_label.pack()
         self.backstep_button.pack()
         self.move_input.pack()
         self.step_manual.pack()
+        self.canv_label.pack()
         self.canvas.pack()
+        self.autoencoder_canv_label.pack()
+        self.autoencoder_canvas.pack()
+        self.diff_canv_label.pack()
+        self.diff_canvas.pack()
         self.frame.pack()
         
         self.root.bind("<KeyRelease-Left>", self.step_left)
@@ -77,28 +87,44 @@ class ControlPanel:
     def _draw_square(self, coords, color, scale, canv):
         return canv.create_rectangle(coords[0]-scale/2, coords[1]-scale/2, coords[0]+scale/2, coords[1]+scale/2, fill=color)
     
-    def draw_canvas(self, weights, renorm=True):
-        max_str = max([abs(option.item()) for option in weights])
-        weights = weights/max_str
-        normed_strs = [option.item()/max_str for option in weights]
+    def _draw_canvas(self, weights, canv, renorm=True, highlight=True):
+        max_str = torch.max(weights).item()
+        min_str = torch.min(weights).item()
+        weights = (weights-min_str)/(max_str-min_str)*2-1
+        normed_strs = [i.item() for i in weights]
         colors = [self.ZERO_COLOR*(1-abs(strength))+self.MIN_COLOR*abs(strength) if strength < 0 else self.ZERO_COLOR*(1-abs(strength))+self.MAX_COLOR*abs(strength) for strength in normed_strs]
         colors = [ints_to_hex(*color) for color in colors]
         
-        self.canvas.delete("all")
-        chosen = torch.argmax(weights)
+        canv.delete("all")
+        chosen = torch.argmax(weights) if highlight else -1
         for i, color in enumerate(colors):
-            self._draw_square((100+DIRECTIONS[i][0]*50, 100+DIRECTIONS[i][1]*50), color, 50, self.canvas)
+            self._draw_square((100+DIRECTIONS[i][0]*50, 100+DIRECTIONS[i][1]*50), color, 50, canv)
             if i == chosen:
-                self.canvas.create_oval(80+DIRECTIONS[i][0]*50, 80+DIRECTIONS[i][1]*50, 120+DIRECTIONS[i][0]*50, 120+DIRECTIONS[i][1]*50, fill="#00ff00")
+                canv.create_oval(80+DIRECTIONS[i][0]*50, 80+DIRECTIONS[i][1]*50, 120+DIRECTIONS[i][0]*50, 120+DIRECTIONS[i][1]*50, fill="#00ff00")
         
     def step_input(self):
         self.step_direction(int(self.move_input.get("1.0", "end-1c").split()[0]))
+    
+    def _get_autoencoder_predicted_move(self, obs, verbose=False):
+        activation = self.q.get_activations(torch.from_numpy(np.transpose(obs, (2, 0, 1))).float())
+        features = self.autoencoder.get_features(activation)
+        if verbose:
+            for i in range(len(features)):
+                if features[i] > 0:
+                    print(f"Feature {i} is active with value {features[i]}!")
+        activation = self.autoencoder.features_to_out(features)
+        out = self.q.activations_to_out(activation)
+        return out
     
     def step(self):
         move = self.q(torch.from_numpy(np.transpose(self.obs, (2, 0, 1))).float()).argmax().item()
         self.step_direction(move)
         predicted_move = self.q(torch.from_numpy(np.transpose(self.obs, (2, 0, 1))).float())
-        self.draw_canvas(predicted_move)
+        self._draw_canvas(predicted_move, self.canvas)
+        autoencoder_predicted_move = self._get_autoencoder_predicted_move(self.obs)
+        self._draw_canvas(autoencoder_predicted_move, self.autoencoder_canvas)
+        predicted_move_diff = autoencoder_predicted_move - predicted_move
+        self._draw_canvas(predicted_move_diff, self.diff_canvas, highlight=False)
     
     def step_left(self, event): self.step_direction(3)
     def step_right(self, event): self.step_direction(2)
